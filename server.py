734f3f85a59fd0ca0f209608193abf7d7c7808c6
vishal-yadavJSX <147474796+vishal-yadavJSX@users.noreply.github.com>
2024-11-21 00:16:47 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
+#portfolio website server
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home_page():
+def main_page():
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
